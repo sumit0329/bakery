57e48f9439831bc9b23d0d9cc50b124abce8fa43
sumit0329 <stha.sls08@gmail.com>
2025-10-10 11:43:40 +1100

    fixing hash

--- users.php
+++ users.php
@@ -49,19 +49,18 @@ if (isset($_POST['add_user'])) {
 }
 
 
-$result = $conn->query("SELECT id, username, password FROM users ORDER BY id ASC");
+$result = $conn->query("SELECT id, username FROM users ORDER BY id ASC");
 if ($result->num_rows > 0) {
     echo "<table>
             <tr>
                 <th>User ID</th>
                 <th>Username</th>
-                <th>Password (hashed)</th>
             </tr>";
     while ($row = $result->fetch_assoc()) {
         echo "<tr>
                 <td>{$row['id']}</td>
                 <td>{$row['username']}</td>
-                <td>{$row['password']}</td>
+                
               </tr>";
     }
     echo "</table>";
